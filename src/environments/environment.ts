58de5a0efee93f49aec8b723a046246054c8f5cd
Sergey Sychev <81991201+sesychev@users.noreply.github.com>
2022-10-26 17:24:57 +0300

    task-7: edit endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,19 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
+    //product: 'https://i8x8oumg0e.execute-api.eu-west-1.amazonaws.com/dev/',
+    //order: 'https://i8x8oumg0e.execute-api.eu-west-1.amazonaws.com/dev/',
+    //import: 'https://i8x8oumg0e.execute-api.eu-west-1.amazonaws.com/dev/',
+    //import: 'https://.execute-api.eu-west-1.amazonaws.com/dev/import',
+    //bff: 'https://i8x8oumg0e.execute-api.eu-west-1.amazonaws.com/dev/',
+    //cart: 'https://i8x8oumg0e.execute-api.eu-west-1.amazonaws.com/dev/',
+    /*
+    product: 'https://201bq42047.execute-api.eu-west-1.amazonaws.com/dev',
+    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://8tu92szicf.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://201bq42047.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    */
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
